9949d68427ecbc7b6f9dc6de75e69ada7a872088
csilvasantin <csilvasantin@MacMini.local>
2026-04-21 23:31:43 +0200

    Add vertical resize for bottom docks

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.21.04.7',
-    build: '20260421-2340',
-    cacheName: 'admiranext-v-26-21-04-7-20260421-2340',
+    version: 'AdmiraNext v.26.21.04.8',
+    build: '20260421-2351',
+    cacheName: 'admiranext-v-26-21-04-8-20260421-2351',
   });
 })();
