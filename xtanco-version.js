edcd61597809647b38afd1947e8e54167b18d53e
csilvasantin <csilvasantin@MacMini.local>
2026-04-21 22:32:17 +0200

    feat: deepen shop simulation and bridge flow [Codex]

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.20.04.5',
-    build: '20260420-2228',
-    cacheName: 'admiranext-v-26-20-04-5-20260420-2228',
+    version: 'AdmiraNext v.26.21.04.3',
+    build: '20260421-1821',
+    cacheName: 'admiranext-v-26-21-04-3-20260421-1821',
   });
 })();
