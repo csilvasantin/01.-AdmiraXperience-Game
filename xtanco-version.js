ccb7967e7e847778c2b1c4df87d89126d9425c4f
csilvasantin <csilvasantin@MacMini.local>
2026-04-21 23:54:19 +0200

    Extend ticket column to panel edge

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.21.04.13',
-    build: '20260422-0035',
-    cacheName: 'admiranext-v-26-21-04-13-20260422-0035',
+    version: 'AdmiraNext v.26.21.04.14',
+    build: '20260422-0042',
+    cacheName: 'admiranext-v-26-21-04-14-20260422-0042',
   });
 })();
