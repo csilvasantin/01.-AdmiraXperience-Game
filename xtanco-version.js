de5384584045f91c03da02a6ebaaedb591153d6a
csilvasantin <csilvasantin@MacMini.local>
2026-04-22 00:04:50 +0200

    Show command help inside store control panel

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.21.04.15',
-    build: '20260422-0058',
-    cacheName: 'admiranext-v-26-21-04-15-20260422-0058',
+    version: 'AdmiraNext v.26.21.04.16',
+    build: '20260422-0110',
+    cacheName: 'admiranext-v-26-21-04-16-20260422-0110',
   });
 })();
