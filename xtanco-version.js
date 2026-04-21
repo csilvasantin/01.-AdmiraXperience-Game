1e854b6a270874a56e82d08d5d0c19b043d62a89
csilvasantin <csilvasantin@MacMini.local>
2026-04-22 00:00:29 +0200

    Fix Telegram clock command polling

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.21.04.14',
-    build: '20260422-0042',
-    cacheName: 'admiranext-v-26-21-04-14-20260422-0042',
+    version: 'AdmiraNext v.26.21.04.15',
+    build: '20260422-0058',
+    cacheName: 'admiranext-v-26-21-04-15-20260422-0058',
   });
 })();
