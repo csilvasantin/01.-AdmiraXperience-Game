5f07968da2ca6c7b2b1b7930a4498092255075c8
csilvasantin <csilvasantin@MacMini.local>
2026-04-21 23:18:38 +0200

    Extend customers dock to bottom

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.21.04.4',
-    build: '20260421-2315',
-    cacheName: 'admiranext-v-26-21-04-4-20260421-2315',
+    version: 'AdmiraNext v.26.21.04.5',
+    build: '20260421-2324',
+    cacheName: 'admiranext-v-26-21-04-5-20260421-2324',
   });
 })();
