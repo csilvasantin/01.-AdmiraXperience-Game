6ee4b47925418cc482b4774a67b6848a54cc73e1
csilvasantin <csilvasantin@MacMini.local>
2026-04-22 00:09:08 +0200

    Clear public game cache on fresh loads

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.21.04.16',
-    build: '20260422-0110',
-    cacheName: 'admiranext-v-26-21-04-16-20260422-0110',
+    version: 'AdmiraNext v.26.21.04.17',
+    build: '20260422-0124',
+    cacheName: 'admiranext-v-26-21-04-17-20260422-0124',
   });
 })();
