742446f076e4045fdbe772cfce1f27cf125e725e
csilvasantin <csilvasantin@MacMini.local>
2026-04-21 23:39:45 +0200

    Auto-grow bottom dock in tall layouts

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.21.04.9',
-    build: '20260421-2359',
-    cacheName: 'admiranext-v-26-21-04-9-20260421-2359',
+    version: 'AdmiraNext v.26.21.04.10',
+    build: '20260422-0008',
+    cacheName: 'admiranext-v-26-21-04-10-20260422-0008',
   });
 })();
