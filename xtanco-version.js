2a0cc8a7c90fa053d0bb2a93652902d89ba011ec
csilvasantin <csilvasantin@MacMini.local>
2026-04-21 23:14:41 +0200

    Polish Admira XP interface shell

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'AdmiraNext v.26.21.04.3',
-    build: '20260421-1821',
-    cacheName: 'admiranext-v-26-21-04-3-20260421-1821',
+    version: 'AdmiraNext v.26.21.04.4',
+    build: '20260421-2315',
+    cacheName: 'admiranext-v-26-21-04-4-20260421-2315',
   });
 })();
